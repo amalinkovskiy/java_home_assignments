0e1678ed65c8ae82ce27e33c2df6d16a6554c338
Alexander Malinkovskiy <amalinkovskiy@gmail.com>
2017-04-09 15:21:22 +0300

    Added several methods

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,30 @@
 package ua.stqa.pft.sandbox;
 
-public class MyFirstProgram{
-	
-public static void main(String[] args){ 
-	System.out.println("Hi, Hero!");
-}
+public class MyFirstProgram {
 
+    public static void main(String[] args) {
+        hello("Hello");
+        hello("Hi");
+        hello("Hola");
+
+        double len = 5.0;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 4.0;
+        double b = 6.0;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    }
+
+    public static void hello(String greeting) {
+        System.out.println(greeting + ", Hero!");
+    }
+
+    public static double area (double l){
+        return l * l;
+    }
+
+    public static double area (double a, double b){
+        return a * b;
+    }
 
 }
