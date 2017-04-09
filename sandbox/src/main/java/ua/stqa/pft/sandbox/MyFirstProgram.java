7e7feee999e87c5b5d07daf18ecbf82983338e4c
Alexander Malinkovskiy <amalinkovskiy@gmail.com>
2017-04-09 20:49:34 +0300

    Added new classes, methods and constructors

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,24 +7,19 @@ public class MyFirstProgram {
         hello("Hi");
         hello("Hola");
 
-        double len = 5.0;
-        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4.0;
-        double b = 6.0;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        Rectangle r = new Rectangle(4.0,6.0);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String greeting) {
         System.out.println(greeting + ", Hero!");
     }
 
-    public static double area (double l){
-        return l * l;
-    }
 
-    public static double area (double a, double b){
-        return a * b;
-    }
+
+
 
 }
