e67ae0bc301f7126e6ce570e0601888a9cf50ade
Alexander Malinkovskiy <amalinkovskiy@gmail.com>
2017-04-23 17:14:35 +0300

    Added deleagtion

---
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,22 @@
+package ua.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by amalinkovskiy on 4/23/2017.
+ */
+public class SessionHelper extends HelperBase {
+
+
+    public SessionHelper(FirefoxDriver wd) {
+
+        super(wd);
+    }
+
+    public void login(String username, String password) {
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.xpath("//form[@id='LoginForm']/input[3]"));
+    }
+}
