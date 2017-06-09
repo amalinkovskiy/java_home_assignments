6df0ff72cc30f60c7e1a897e864ba3e5471038d2
Alexander Malinkovskiy <amalinkovskiy@gmail.com>
2017-06-09 10:11:02 +0300

    changed rus symbols to eng

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,10 +8,10 @@ public class MyFirstProgram {
         hello("Hola");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        System.out.println("square of kavdrat " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4.0,6.0);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+        System.out.println("square of priamougolnik " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String greeting) {
